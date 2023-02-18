19423a9029118a56f654cae636477f2b9672a533
Delma <delmant1985@gmail.com>
2023-02-18 13:50:06 +0300

    fix annotation in getJoke()

--- app/src/main/java/com/example/jokeapp/data/database/JokeDao.kt
+++ app/src/main/java/com/example/jokeapp/data/database/JokeDao.kt
@@ -4,14 +4,15 @@ import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import retrofit2.http.GET
 
 @Dao
 interface JokeDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertJoke(joke: JokeInfoDbModel)
+    fun insertJoke(joke: JokeInfoDbModel)
 
-    @GET("SELECT * FROM joke_db")
+    @Query("SELECT * FROM joke_db")
     fun getJoke(): LiveData<JokeInfoDbModel>
 }
--- app/src/main/java/com/example/jokeapp/data/database/JokeDatabase.kt
+++ app/src/main/java/com/example/jokeapp/data/database/JokeDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [JokeInfoDbModel::class], version = 1)
+@Database(entities = [JokeInfoDbModel::class], version = 2)
 abstract class JokeDatabase : RoomDatabase() {
 
     abstract fun JokeDao(): JokeDao
