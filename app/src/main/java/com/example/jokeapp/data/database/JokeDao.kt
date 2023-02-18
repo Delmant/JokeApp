3af876b13d781934d5812db629f780d34c434acf
Delma <delmant1985@gmail.com>
2023-02-18 13:43:33 +0300

    end of work with the date base. creating joke database

--- app/src/main/java/com/example/jokeapp/data/database/JokeDao.kt
+++ app/src/main/java/com/example/jokeapp/data/database/JokeDao.kt
@@ -1,4 +1,4 @@
-package com.example.jokeapp.data
+package com.example.jokeapp.data.database
 
 import androidx.lifecycle.LiveData
 import androidx.room.Dao
---
+++ app/src/main/java/com/example/jokeapp/data/database/JokeDatabase.kt
@@ -0,0 +1,34 @@
+package com.example.jokeapp.data.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [JokeInfoDbModel::class], version = 1)
+abstract class JokeDatabase : RoomDatabase() {
+
+    abstract fun JokeDao()
+
+    companion object {
+
+        private var db: JokeDatabase? = null
+        private const val DB_NAME = "joke.db"
+        private val LOCK = Any()
+
+        fun getInstance(context: Context): JokeDatabase {
+            synchronized(LOCK) {
+                db?.let {
+                    return it
+                }
+                val instance = Room.databaseBuilder(
+                    context,
+                    JokeDatabase::class.java,
+                    DB_NAME
+                ).build()
+                db = instance
+                return instance
+            }
+        }
+    }
+}
