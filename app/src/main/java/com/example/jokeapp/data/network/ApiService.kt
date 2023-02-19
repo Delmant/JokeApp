aba4443ea859e9bd7e2bffcca80c3005a80fc5d0
Delma <delmant1985@gmail.com>
2023-02-19 20:50:34 +0300

    add Query parameters "type" into suspend function getJoke()

--- app/src/main/java/com/example/jokeapp/data/network/ApiService.kt
+++ app/src/main/java/com/example/jokeapp/data/network/ApiService.kt
@@ -2,11 +2,16 @@ package com.example.jokeapp.data.network
 
 import com.example.jokeapp.data.network.model.JokeInfoDto
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface ApiService {
 
-    @GET("Dark?type=single")
+    @GET("Dark")
     suspend fun getJoke(
-
+        @Query(QUERY_PARAM_TYPE) type: String
     ): JokeInfoDto
+
+    companion object {
+        private const val QUERY_PARAM_TYPE = "type"
+    }
 }
