57fc59c15d024584d104c6c43d8b6481b43db78c
Delma <delmant1985@gmail.com>
2023-02-25 19:33:58 +0300

    finish test part of JokeViewModel

--- app/src/main/java/com/example/jokeapp/presentation/JokeViewModel.kt
+++ app/src/main/java/com/example/jokeapp/presentation/JokeViewModel.kt
@@ -3,7 +3,18 @@ package com.example.jokeapp.presentation
 import android.app.Application
 import android.content.Context
 import androidx.lifecycle.AndroidViewModel
+import com.example.jokeapp.data.repository.JokeRepositoryImpl
+import com.example.jokeapp.domain.GetJokeUseCase
+import com.example.jokeapp.domain.LoadJokeUseCase
 
 class JokeViewModel(application: Application): AndroidViewModel(application) {
 
+    private val repository = JokeRepositoryImpl(application)
+
+    private val getJokeUseCase = GetJokeUseCase(repository)
+    private val loadJokeUseCase = LoadJokeUseCase(repository)
+
+    init {
+        loadJokeUseCase()
+    }
 }
